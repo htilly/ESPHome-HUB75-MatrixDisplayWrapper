3e16f7a8b555ae6716b6c25764483f10aebee880
Henrik Tilly <code@tilly.se>
2025-04-10 20:32:11 +0200

    Comment out unused I2S speed cases in MatrixDisplay configuration

--- components/hub75_matrix_display/matrix_display.cpp
+++ components/hub75_matrix_display/matrix_display.cpp
@@ -127,15 +127,18 @@ namespace esphome
             case HUB75_I2S_CFG::clk_speed::HZ_8M:
                 ESP_LOGCONFIG(TAG, "I2SSpeed: HZ_8M");
                 break;
-            case HUB75_I2S_CFG::clk_speed::HZ_10M:
-                ESP_LOGCONFIG(TAG, "I2SSpeed: HZ_10M");
-                break;
-            case HUB75_I2S_CFG::clk_speed::HZ_15M:
-                ESP_LOGCONFIG(TAG, "I2SSpeed: HZ_15M");
-                break;
-            case HUB75_I2S_CFG::clk_speed::HZ_20M:
-                ESP_LOGCONFIG(TAG, "I2SSpeed: HZ_20M");
-                break;
+            //case HUB75_I2S_CFG::clk_speed::HZ_10M:
+            //    ESP_LOGCONFIG(TAG, "I2SSpeed: HZ_10M");
+            //    break;
+            //case HUB75_I2S_CFG::clk_speed::HZ_15M:
+            //    ESP_LOGCONFIG(TAG, "I2SSpeed: HZ_15M");
+            //    break;
+            //case HUB75_I2S_CFG::clk_speed::HZ_20M:
+            //    ESP_LOGCONFIG(TAG, "I2SSpeed: HZ_20M");
+            //    break;
+            //default:
+            //    ESP_LOGCONFIG(TAG, "I2SSpeed: Unknown");
+            //    break;
             }
 
             ESP_LOGCONFIG(TAG, "Latch blanking: %i", dma_display_->getCfg().latch_blanking);
